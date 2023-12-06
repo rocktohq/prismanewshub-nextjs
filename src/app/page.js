4dfbfc80ed9293b0cc4edab3ee12d973fea407ce
rocktohq <itzmonir@gmail.com>
2023-12-06 23:12:31 +0600

    Home Page Updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,18 @@
+export const metadata = {
+  title: "PrismaNewsHub - Home",
+};
+
 const HomePage = () => {
-  return <div>Hello From Home Page</div>;
+  return (
+    <div className="max-w-screen-xl mx-auto p-10 my-10 rounded-xl shadow-lg px-2">
+      <h1 className="text-center text-4xl font-bold">
+        Welcome to PrismaNewsHub
+      </h1>
+      <p className="mt-2 text-neutral-500 text-center font-medium">
+        Read latest news with your pc or mobile any time.
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
